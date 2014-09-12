8de8dfaa8de8418cacc45f16206fd83ce717aa23
Per Arneng <per.arneng@scalebit.com>
2014-09-12 21:32:24 +0200

    implemented the parsing

--- vpgen.js
+++ vpgen.js
@@ -2,18 +2,81 @@ var vpgen;
 (function (vpgen) {
     function onTextChange(e) {
         var textArea = document.getElementById("inp");
+        var textAreaOut = document.getElementById("outp");
+
         var txt = textArea.value;
 
         var parser = new Parser();
-        parser.parse(txt);
+        var laps = parser.parse(txt);
+
+        var errorString = "";
+        for (var lapIndex in laps) {
+            var lap = laps[lapIndex];
+            if (lap.error != undefined) {
+                errorString += "line " + lap.lineNr + ": the string '" + lap.original + "' failed with '" + lap.error + "'\n";
+            }
+        }
+
+        textAreaOut.value = errorString;
+
+        console.log(laps);
     }
     vpgen.onTextChange = onTextChange;
 
+    var Lap = (function () {
+        function Lap(lineNr, original, distance, time, err) {
+            this.lineNr = lineNr;
+            this.error = err;
+            this.original = original;
+            this.distance = distance;
+            this.time = time;
+        }
+        return Lap;
+    })();
+
     var Parser = (function () {
         function Parser() {
+            this.lineRegexp = /^(\d\d):(\d\d):(\d\d)\s+(\d+)\s*(km|k|m)?$/;
         }
         Parser.prototype.parse = function (str) {
-            alert("xx:" + str);
+            var laps = [];
+
+            var lineNr = 0;
+            var lines = str.split("\n");
+            for (var lineIndex in lines) {
+                lineNr++;
+
+                var line = lines[lineIndex];
+
+                if (line.length == 0 || (/^\s+$/).exec(line) != undefined) {
+                    continue;
+                }
+
+                var match = this.lineRegexp.exec(line);
+
+                if (match == undefined) {
+                    var lap = new Lap(lineNr, line, 0, 0, "Not in the format '00:00:00 0 (km|k|m)'");
+                } else {
+                    var hours = +match[1];
+                    var minutes = +match[2];
+                    var seconds = +match[3];
+                    var distance = +match[4];
+                    var unit = match[5];
+
+                    var time = hours * 60 * 60 + minutes * 60 + seconds;
+                    if (unit === "k" || unit === "km") {
+                        distance = distance * 1000;
+                    }
+
+                    var lap = new Lap(lineNr, line, distance, time, null);
+                }
+
+                laps.push(lap);
+
+                console.log("" + lap);
+            }
+
+            return laps;
         };
         return Parser;
     })();
--- vpgen.ts
+++ vpgen.ts
@@ -4,17 +4,96 @@ module vpgen {
 	
 	export function onTextChange(e) {
 		
-		var textArea = document.getElementById("inp");
+		var textArea = <HTMLInputElement>document.getElementById("inp");
+		var textAreaOut = <HTMLInputElement>document.getElementById("outp");
+
 		var txt:string = textArea.value;
 
 		var parser:Parser = new Parser();
-		parser.parse(txt);
+		var laps = parser.parse(txt);
+		
+		var errorString:string = ""; 
+		for (var lapIndex in laps) {
+			var lap = laps[lapIndex];
+			if (lap.error != undefined) {
+				errorString += "line " + lap.lineNr + ": the string '" + lap.original + 
+							   "' failed with '" + lap.error + "'\n"; 
+			}
+		}
+		
+		textAreaOut.value = errorString;
+		
+		console.log(laps);
+	}
+
+	class Lap {
+	
+		distance:number
+		time:number
+		original:string
+		lineNr:number
+		error:string
+		
+		constructor(lineNr:number, original:string, distance:number,
+					time:number, err:string) {
+					
+			this.lineNr = lineNr
+			this.error = err;
+			this.original = original;
+			this.distance = distance;
+			this.time = time;
+		}
+		
 	}
 
 	class Parser {
 
-		parse(str:string) {
-			alert("xx:" + str);
+		lineRegexp:RegExp = /^(\d\d):(\d\d):(\d\d)\s+(\d+)\s*(km|k|m)?$/; 
+
+		parse(str:string) : Lap[] {
+
+			var laps:Lap[] = [];
+
+			var lineNr:number = 0;
+			var lines = str.split("\n");
+			for (var lineIndex in lines) {
+				lineNr++;
+								
+				var line:string = lines[lineIndex];
+				
+				if (line.length == 0 || (/^\s+$/).exec(line) != undefined) {
+					continue;
+				}
+				
+				
+				var match = this.lineRegexp.exec(line);
+				
+				if (match == undefined) {
+					var lap:Lap = new Lap(lineNr, line, 0, 0,
+										  "Not in the format '00:00:00 0 (km|k|m)'");
+				} else {
+				
+					var hours:number = +match[1];
+					var minutes:number = +match[2];
+					var seconds:number = +match[3];
+					var distance:number = +match[4];
+					var unit:string = match[5]
+
+					var time:number = hours*60*60 + minutes*60 + seconds;
+					if (unit === "k" || unit === "km") {
+						distance = distance*1000;
+					} 
+				
+					var lap:Lap = new Lap(lineNr, line, distance, time, null);
+				}
+				
+				laps.push(lap);
+				
+				console.log("" + lap);
+				
+			}
+
+			return laps;
 		}
 
 	}
